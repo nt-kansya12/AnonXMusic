5202f9a716f7f928ed975d60375dd7b99199459d
nt-kansya12 <153280158+nt-kansya12@users.noreply.github.com>
2023-12-20 23:36:09 +0700

    Adira userbot.py
    Signed-off-by: nt-kansya12 <153280158+nt-kansya12@users.noreply.github.com>

--- AnonXMusic/core/userbot.py
+++ AnonXMusic/core/userbot.py
@@ -47,7 +47,7 @@ class Userbot(Client):
         )
 
     async def start(self):
-        LOGGER(__name__).info(f"Starting Assistants Berlin...")
+        LOGGER(__name__).info(f"Starting Assistants Adira...")
         if config.STRING1:
             await self.one.start()
             try:
@@ -57,7 +57,7 @@ class Userbot(Client):
                 pass
             assistants.append(1)
             try:
-                await self.one.send_message(config.LOGGER_ID, "Assistant Berlin Started")
+                await self.one.send_message(config.LOGGER_ID, "Assistant Adira Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Akun 1 Gagal Akses Ke Log groups/channel. Tambahin dulu akun assistant lu ke log groups terus lu adminin mek!"
@@ -78,7 +78,7 @@ class Userbot(Client):
                 pass
             assistants.append(2)
             try:
-                await self.two.send_message(config.LOGGER_ID, "Assistant Berlin Started")
+                await self.two.send_message(config.LOGGER_ID, "Assistant Adira Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Akun 2 Gagal Akses Ke Log groups/channel. Tambahin dulu akun assistant lu ke log groups terus lu adminin mek!"
@@ -99,7 +99,7 @@ class Userbot(Client):
                 pass
             assistants.append(3)
             try:
-                await self.three.send_message(config.LOGGER_ID, "Assistant Berlin Started")
+                await self.three.send_message(config.LOGGER_ID, "Assistant Adira Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Akun 3 Gagal Akses Ke Log groups/channel. Tambahin dulu akun assistant lu ke log groups terus lu adminin mek!! "
@@ -120,7 +120,7 @@ class Userbot(Client):
                 pass
             assistants.append(4)
             try:
-                await self.four.send_message(config.LOGGER_ID, "Assistant Berlin Started")
+                await self.four.send_message(config.LOGGER_ID, "Assistant Adira Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Akun 4 Gagal Akses Ke Log groups/channel. Tambahin dulu akun assistant lu ke log groups terus lu adminin mek!! "
@@ -141,7 +141,7 @@ class Userbot(Client):
                 pass
             assistants.append(5)
             try:
-                await self.five.send_message(config.LOGGER_ID, "Assistant Berlin Started")
+                await self.five.send_message(config.LOGGER_ID, "Assistant Adira Started")
             except:
                 LOGGER(__name__).error(
                     "Assistant Akun 5 Gagal Akses Ke Log groups/channel. Tambahin dulu akun assistant lu ke log groups terus lu adminin mek!! "
